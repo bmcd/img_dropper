c6272af789f108121b5f72e4f24dceac6972775b
bmcd87 <bradmcdermott@gmail.com>
2013-10-17 10:24:35 -0400

    Adds email address to send forgot password emails from

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,10 @@ ImgurClone::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   
-  config.action_mailer.default_url_options = { host: "imgdropper.com"}
+  config.action_mailer.default_url_options = { host: "imgdropper.com" }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
 
   #AWS
   config.paperclip_defaults = {
